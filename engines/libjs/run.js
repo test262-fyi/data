3a3de33c25a23c02c1e8569670f6feea2f99936e
Oliver Medhurst <honk@goose.icu>
2025-07-10 23:08:19 +0100

    libjs: fix runs by using newly renamed cli option

--- engines/libjs/run.js
+++ engines/libjs/run.js
@@ -1,7 +1,7 @@
 import { $, $$ } from '../../cli.js';
 
 export default (file, module = false) => {
-  const args = [ '--disable-ansi-colors', '--no-syntax-highlight', '--disable-source-location-hints', '--disable-debug-output', '--use-test262-global', '--disable-string-quotes', file ];
+  const args = [ '--disable-ansi-colors', '--no-syntax-highlight', '--disable-source-location-hints', '--disable-debug-output', '--use-test262-global', '--raw-strings', file ];
   if (module) args.unshift('-m');
 
   return $$(`./libjs`, args);
