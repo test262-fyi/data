fa8507f1952921f3217b4de3a15ca6724e86ebab
Oliver Medhurst <honk@goose.icu>
2025-07-10 23:07:58 +0100

    boa: use new nightly builds

--- engines/boa/setup.js
+++ engines/boa/setup.js
@@ -1,16 +1,17 @@
 import fs from 'node:fs';
+import { Readable } from 'node:stream';
+import { finished } from 'node:stream/promises';
 import { $ } from '../../cli.js';
 
-const buildDir = process.env.HOME + '/boa';
 export default async () => {
-  console.log('building boa... (this will take a while)');
-  $(`rm -rf ${buildDir}`);
-  $(`git clone https://github.com/boa-dev/boa.git ${buildDir} --depth=1`);
-  const version = $(`git -C ${buildDir} rev-parse HEAD`).trim().slice(0, 7);
+  const headers = process.env.GITHUB_TOKEN ? { Authorization: `Bearer ${process.env.GITHUB_TOKEN}` } : {};
+  const runs = (await (await fetch('https://api.github.com/repos/boa-dev/boa/actions/runs?per_page=100', { headers })).json()).workflow_runs;
+  const version = (runs.find(x => x.name === 'Nightly Build') ?? runs.at(-1)).head_sha.slice(0, 7);
 
-  $(`cd ${buildDir} && rustup run stable cargo build --release -j32`);
-  $(`cp -f ${buildDir}/target/release/boa ./boa`);
-  $(`rm -rf ${buildDir}`);
+  const { body } = await fetch(`https://github.com/boa-dev/boa/releases/download/nightly/boa-aarch64-unknown-linux-gnu`);
+  await finished(Readable.fromWeb(body).pipe(fs.createWriteStream('boa')));
+
+  $('chmod +x boa');
 
   return { version };
 };
