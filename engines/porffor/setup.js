965671f7c7f9d20dae39675f76fd95c2a9a2cdb4
Oliver Medhurst <honk@goose.icu>
2025-09-05 00:54:38 +0100

    engines/porffor: use git not npm

--- engines/porffor/setup.js
+++ engines/porffor/setup.js
@@ -1,15 +1,12 @@
 import fs from 'node:fs';
-import { Readable } from 'node:stream';
-import { finished } from 'node:stream/promises';
 import { $ } from '../../cli.js';
 
 export default async () => {
-  $('mkdir -p ./porffor/node_modules');
-  $('npm install --prefix ./porffor porffor@latest');
+  $(`git clone https://github.com/CanadaHonk/porffor.git porffor --depth=1`);
 
   return {
-    version: $('node ./porffor/node_modules/porffor/runtime/index.js --version').trim(),
-    preludes: (await (await fetch('https://raw.githubusercontent.com/CanadaHonk/porffor/main/test262/harness.js')).text()).split('///').reduce((acc, x) => {
+    version: $('node ./porffor/runtime/index.js --version').trim(),
+    preludes: fs.readFileSync('./porffor/test262/harness.js', 'utf8').split('///').reduce((acc, x) => {
       const [ k, ...content ] = x.split('\n');
       acc[k.trim()] = content.join('\n').trim() + '\n';
       return acc;
