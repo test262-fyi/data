b3f027889df1f59eb612b6dcd79d7034fc8d91ca
Oliver Medhurst <honk@goose.icu>
2025-09-04 17:40:58 +0100

    engines/jsc: pin commit

--- engines/jsc/setup.js
+++ engines/jsc/setup.js
@@ -13,7 +13,10 @@ export default async () => {
   $(`git clone https://github.com/WebKit/WebKit.git ${buildDir} --depth=1`);
   const version = $(`git -C ${buildDir} rev-parse HEAD`).trim().slice(0, 7);
 
-  $(`${buildDir}/Tools/Scripts/build-jsc --jsc-only --cmakeargs="-DUSE_64KB_PAGE_BLOCK=1"`);
+  // *deep sigh* https://bugs.webkit.org/show_bug.cgi?id=298057
+  $(`cd ${buildDir}; git fetch --depth 1 origin 952e58054d0fd2b45264d09bf2ef391d1ddb1f3b; git checkout 952e58054d0fd2b45264d09bf2ef391d1ddb1f3b`);
+
+  $(`${buildDir}/Tools/Scripts/build-jsc --jsc-only --cmakeargs="-DUSE_64KB_PAGE_BLOCK=1" --makeargs="-j32"`);
   $(`cp -rf ${buildDir}/WebKitBuild/JSCOnly/Release ./jsc`);
 
   return { version };
